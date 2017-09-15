54871f5fa787331699955ad3e88692de81c07428
Muhammad Wahyudin <fnp90spy@gmail.com>
2017-09-15 10:23:30 +0700

    Placeholder for development

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
-
+    $("html").easeScroll({
+      frameRate: 60,
+      animationTime: 1000,
+      stepSize: 120,
+      pulseAlgorithm: 1,
+      pulseScale: 8,
+      pulseNormalize: 1,
+      accelerationDelta: 20,
+      accelerationMax: 1,
+      keyboardSupport: true,
+      arrowScroll: 50,
+      touchpadSupport: true,
+      fixedBackground: false
+    });
+    
     $('.modal-trigger').leanModal();
     $('#push,section').pushpin({ top:$('#push').height() });
 
@@ -25,3 +39,10 @@ $(document).ready(function(){
         //   $("#navbar").removeClass('transparent z-depth-0').addClass('grey darken-4 z-depth-1');
         // }
       });
+
+
+      $("#btn-in").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#main").offset().top
+        }, 1500);
+    });
